df126f6ea419bfbfb36c2899a82ffcd4dbaa1692
Robert Söderbjörn <robert.soderbjorn@framna.com>
2026-06-20 15:40:01 +0200

    Android: Bump version to 1.1.1 (4).

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "se.soderbjorn.termtastic.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
     }
 
     buildFeatures {
